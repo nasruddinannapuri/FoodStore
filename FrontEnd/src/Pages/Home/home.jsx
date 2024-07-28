0186b9a6a76de4e3a7018c7610c84145e2226adb
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-07-28 16:39:25 +0530

    "Added FoodDisplay component to Home page and wrapped App with StoreContextProvider in main.jsx"

--- FrontEnd/src/Pages/Home/home.jsx
+++ FrontEnd/src/Pages/Home/home.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react'
 import './home.css'
 import Header from "../../Components/Header/header"
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 const home = () => {
 
   const [category, setCategory] = useState("All");
-
+  
   return (
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
