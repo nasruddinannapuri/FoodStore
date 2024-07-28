05435e7ba3409935dbaaec967e697011651eddf8
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-07-28 11:17:54 +0530

    "Added Header component to Home page and created Header component with JSX and CSS"

--- FrontEnd/src/Pages/Home/home.jsx
+++ FrontEnd/src/Pages/Home/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './home.css'
+import Header from "../../Components/Header/header"
 
 const home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
