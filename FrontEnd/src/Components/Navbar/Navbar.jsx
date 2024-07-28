4b74ceee68e7cf078919c996c136198d29021007
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-07-28 23:14:42 +0530

    "Updated Navbar component to display cart amount, added Cart page with item list and totals, and modified StoreContext to include getTotalCartAmount function"

--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 /* for Underline effect for the menu bar */
     const [menu,setMenu] = useState("home");
+
+    const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+      <Link to="/"><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className="navbar-menu">
         <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
@@ -18,8 +22,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className='navbar-search-icon'>
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+            <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign-In</button>    
       </div>
