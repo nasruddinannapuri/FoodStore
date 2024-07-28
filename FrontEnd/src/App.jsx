bb0458016ac03dbabf0d3a4c694b69cfe64b701b
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-07-28 22:08:13 +0530

    "Added login popup feature to App component and Navbar component"

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from "./Pages/Home/home"
 import Cart from "./Pages/Cart/cart"
 import PlaceOrder from "./Pages/PlaceOrder/placeorder"
 import Footer from './Components/Footer/Footer'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 /* for Underline effect for the menu bar */
     const [menu,setMenu] = useState("home");
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo' />
       <ul className="navbar-menu">
-        <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
         <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
         <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
@@ -19,11 +19,9 @@ const Navbar = () => {
         <img src={assets.search_icon} alt="" />
         <div className='navbar-search-icon'>
             <img src={assets.basket_icon} alt="" />
-            <div className="dot">
-
-            </div>
+            <div className="dot"></div>
         </div>
-        <button>Sign-In</button>    
+        <button onClick={()=>setShowLogin(true)}>Sign-In</button>    
       </div>
     </div>
   )
