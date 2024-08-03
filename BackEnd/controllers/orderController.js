c4a57652f90cf6ecc497e0c756ed927c01246438
Annapuri Nasruddin <118391336+nasruddinannapuri@users.noreply.github.com>
2024-08-03 11:19:00 +0530

    Update orderController.js

--- BackEnd/controllers/orderController.js
+++ BackEnd/controllers/orderController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const frontend_url = "http://localhost:5173";
+const frontend_url = "https://foodstore-frontend-cffl.onrender.com/";
 
 const placeOrder = async (req, res) => {
   try {
