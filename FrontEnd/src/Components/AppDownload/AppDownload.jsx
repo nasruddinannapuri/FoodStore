73729512b4775d1e47645848c62b8d3ab5b0c46a
Annapuri Nasruddin <118391336+nasruddinannapuri@users.noreply.github.com>
2025-03-30 08:46:26 +0530

    Update AppDownload.jsx

--- FrontEnd/src/Components/AppDownload/AppDownload.jsx
+++ FrontEnd/src/Components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
         <p>
-            For Better Experience Download <br /> Mango App
+            For Better Experience Download <br /> Tomato App
         </p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
